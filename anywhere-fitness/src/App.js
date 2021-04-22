4c4f89ff866bd5128311b33e210ed9eb63ca142e
Greg Brewton <gbrewton1980@gmail.com>
2021-04-21 20:16:08 -0600

    remove unneccesary app wrapper div

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -7,43 +7,41 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <header id="header">
-          <h1>Anywhere Fitness</h1>
-          <nav className="navbar">
-            <ul className="navlist">
-              <li className="navItem">
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/signup">Sign Up</Link>
-              </li>
-              <li>
-                <Link to="/login">Log In</Link>
-              </li>
-              <li>
-                <Link to="/search">Search Classes</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <Switch>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-          <Route path="/search">
-            <Search />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Router>
+      <header id="header">
+        <h1>Anywhere Fitness</h1>
+        <nav className="navbar">
+          <ul className="navlist">
+            <li className="navItem">
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Log In</Link>
+            </li>
+            <li>
+              <Link to="/search">Search Classes</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Switch>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
