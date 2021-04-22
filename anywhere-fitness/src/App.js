0c7ca79d9d3fadbf428bc1514eb8a76f42a640b0
Greg Brewton <gbrewton1980@gmail.com>
2021-04-21 19:40:29 -0600

    Creates Search component

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,10 +1,12 @@
 import Home from "./components/Home";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Anywhere Fitness</h1>
       <Home />
+      <Search />
     </div>
   );
 }
---
+++ anywhere-fitness/src/components/Search.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Search = (props) => {
+  return <div id="search"></div>;
+};
+
+export default Search;
