3d53096352e1aeef3092568337003fa9970fc753
Greg Brewton <gbrewton1980@gmail.com>
2021-04-21 20:14:55 -0600

    Adds navigation

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,18 +1,33 @@
-import AddClassForm from "./components/AddClassForm";
-import ClassDetail from "./components/ClassDetail";
-import EditClassForm from "./components/EditClassForm";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Search from "./components/Search";
 import Signup from "./components/Signup";
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
       <Router>
+        <header id="header">
+          <h1>Anywhere Fitness</h1>
+          <nav className="navbar">
+            <ul className="navlist">
+              <li className="navItem">
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/signup">Sign Up</Link>
+              </li>
+              <li>
+                <Link to="/login">Log In</Link>
+              </li>
+              <li>
+                <Link to="/search">Search Classes</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <Switch>
           <Route path="/signup">
             <Signup />
@@ -22,15 +37,6 @@ function App() {
           </Route>
           <Route path="/login">
             <Login />
-          </Route>
-          <Route path="/editclassform">
-            <EditClassForm />
-          </Route>
-          <Route>
-            <ClassDetail />
-          </Route>
-          <Route>
-            <AddClassForm />
           </Route>
           <Route path="/">
             <Home />
