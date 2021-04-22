2c917fc517df95a39b16618d85e09b6a9e9b81d7
Greg Brewton <gbrewton1980@gmail.com>
2021-04-21 20:01:49 -0600

    Adds routing

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -6,17 +6,37 @@ import Login from "./components/Login";
 import Search from "./components/Search";
 import Signup from "./components/Signup";
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Anywhere Fitness</h1>
-      <Home />
-      <Search />
-      <Login />
-      <Signup />
-      <AddClassForm />
-      <EditClassForm />
-      <ClassDetail />
+      
+      <Router>
+        <Switch>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/editclassform">
+            <EditClassForm />
+          </Route>
+          <Route>
+            <ClassDetail />
+          </Route>
+          <Route>
+            <AddClassForm />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
