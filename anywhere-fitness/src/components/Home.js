e3bb322ad117c01e193209da123a66f4ccbfc21c
Greg Brewton <gbrewton1980@gmail.com>
2021-04-26 19:49:26 -0600

    Initial branch commit

--- anywhere-fitness/src/components/Home.js
+++ anywhere-fitness/src/components/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Home = (props) => {
-    return ( <div id="home">Home Component</div> );
+    return ( <div id="home">
+        Authorized User Landing Page
+    </div> );
 }
  
 export default Home;
