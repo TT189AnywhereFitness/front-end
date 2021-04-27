36c439c2fcc35d2138ea0b233b1e8df2bbeaecd0
Greg Brewton <gbrewton1980@gmail.com>
2021-04-26 20:47:06 -0600

    Displays list of dummy data from array with delay

--- anywhere-fitness/src/components/ClassDetail.js
+++ anywhere-fitness/src/components/ClassDetail.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ClassDetail = (props) => {
-  return <div id="classDetail">ClassDetail component</div>;
+  const {name, description} = props;
+  return <div id="classDetail">
+    <h1>{name}</h1>
+    <p>{description}</p>
+  </div>;
 };
 
 export default ClassDetail;
--- anywhere-fitness/src/components/Home.js
+++ anywhere-fitness/src/components/Home.js
@@ -1,53 +1,96 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
+import ClassDetail from "./ClassDetail";
 
-
+// to simulate network load time
+const fakeDelayDuration = 1500;
 
 const Home = (props) => {
+  const dummyClassList = [
+    {
+      name: "Lorem Ipsum",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu cursus vitae congue mauris rhoncus aenean. Arcu vitae elementum curabitur vitae nunc. Enim ut tellus elementum sagittis vitae et leo. Enim diam vulputate ut pharetra sit amet aliquam. Morbi tristique senectus et netus et malesuada. Euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis. Orci ac auctor augue mauris augue. Commodo viverra maecenas accumsan lacus vel facilisis volutpat est. Parturient montes nascetur ridiculus mus. In mollis nunc sed id semper risus in. Aliquet enim tortor at auctor urna. Amet dictum sit amet justo donec. Feugiat in ante metus dictum at tempor commodo ullamcorper a. Elementum tempus egestas sed sed. Amet consectetur adipiscing elit duis tristique sollicitudin nibh. Nisl purus in mollis nunc sed. Natoque penatibus et magnis dis. Hac habitasse platea dictumst vestibulum rhoncus. Adipiscing elit duis tristique sollicitudin nibh sit amet.",
+    },
+    {
+      name: "Hipster Ipsum ",
+      description:
+        "Bespoke bicycle rights master cleanse selvage street art, pitchfork pork belly taiyaki pinterest activated charcoal small batch offal. Dreamcatcher next level tattooed listicle vice deep v PBR&B locavore intelligentsia normcore. Kitsch literally knausgaard, migas roof party tacos viral banjo copper mug kinfolk chambray direct trade iceland YOLO hexagon. Crucifix intelligentsia microdosing quinoa, truffaut kale chips hexagon chartreuse seitan pabst fam.",
+    },
+    {
+      name: "Bacon Ipsum",
+      description:
+        "Spicy jalapeno biltong tongue bacon turducken chuck prosciutto shoulder spare ribs tenderloin beef ribs chislic alcatra corned beef hamburger. Spare ribs sausage bacon ham, chuck burgdoggen beef ribs ham hock pig frankfurter. Bresaola corned beef salami chicken, porchetta ham pork chop turkey bacon spare ribs cupim pastrami short loin biltong. Leberkas pork chop tri-tip jerky, andouille rump beef sirloin sausage shankle hamburger tail cow.",
+    },
+    {
+      name: "Delorean Ipsum",
+      description:
+        "I'm too loud. I can't believe it. I'm never gonna get a chance to play in front of anybody. Watch it, Goldie. Oh. Ah well, sort of. You know Marty, I'm gonna be very sad to see you go. You've really mad a difference in my life, you've given me something to shoot for. Just knowing, that I'm gonna be around to se 1985, that I'm gonna succeed in this. That I'm gonna have a chance to travel through time. It's going to be really hard waiting 30 years before I could talk to you about everything that's happened in the past few days. I'm really gonna miss you, Marty.",
+    },
+    {
+      name: "Busey Ipsum",
+      description:
+        "Busey ipsum dolor sit amet. These kind of things only happen for the first time once.It's good to yell at people and tell people that you're from Tennesee, so that way you'll be safe.The best way to communicate is compatible. Compatible communication is listening with open ears and an open mind, and not being fearful or judgemental about what you're hearing.",
+    },
+    {
+      name: "Samuel L Ipsum",
+      description:
+        "Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.",
+    },
+    {
+      name: "Kitty Ipsum",
+      description:
+        "Chase ball of string eat plants, meow, and throw up because I ate plants going to catch the red dot today going to catch the red dot today. I could pee on this if I had the energy. Chew iPad power cord steal the warm chair right after you get up for purr for no reason leave hair everywhere, decide to want nothing to do with my owner today.",
+    },
+  ];
 
-    const dummyClassList = [{
-        name: "Lorem Ipsum",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu cursus vitae congue mauris rhoncus aenean. Arcu vitae elementum curabitur vitae nunc. Enim ut tellus elementum sagittis vitae et leo. Enim diam vulputate ut pharetra sit amet aliquam. Morbi tristique senectus et netus et malesuada. Euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis. Orci ac auctor augue mauris augue. Commodo viverra maecenas accumsan lacus vel facilisis volutpat est. Parturient montes nascetur ridiculus mus. In mollis nunc sed id semper risus in. Aliquet enim tortor at auctor urna. Amet dictum sit amet justo donec. Feugiat in ante metus dictum at tempor commodo ullamcorper a. Elementum tempus egestas sed sed. Amet consectetur adipiscing elit duis tristique sollicitudin nibh. Nisl purus in mollis nunc sed. Natoque penatibus et magnis dis. Hac habitasse platea dictumst vestibulum rhoncus. Adipiscing elit duis tristique sollicitudin nibh sit amet."
-    },{
-        name: "Hipster Ipsum ",
-        description: "Bespoke bicycle rights master cleanse selvage street art, pitchfork pork belly taiyaki pinterest activated charcoal small batch offal. Dreamcatcher next level tattooed listicle vice deep v PBR&B locavore intelligentsia normcore. Kitsch literally knausgaard, migas roof party tacos viral banjo copper mug kinfolk chambray direct trade iceland YOLO hexagon. Crucifix intelligentsia microdosing quinoa, truffaut kale chips hexagon chartreuse seitan pabst fam."
-    },{
-        name: "Bacon Ipsum",
-        description: "Spicy jalapeno biltong tongue bacon turducken chuck prosciutto shoulder spare ribs tenderloin beef ribs chislic alcatra corned beef hamburger. Spare ribs sausage bacon ham, chuck burgdoggen beef ribs ham hock pig frankfurter. Bresaola corned beef salami chicken, porchetta ham pork chop turkey bacon spare ribs cupim pastrami short loin biltong. Leberkas pork chop tri-tip jerky, andouille rump beef sirloin sausage shankle hamburger tail cow."
-    },{
-        name: "Delorean Ipsum",
-        description: "I'm too loud. I can't believe it. I'm never gonna get a chance to play in front of anybody. Watch it, Goldie. Oh. Ah well, sort of. You know Marty, I'm gonna be very sad to see you go. You've really mad a difference in my life, you've given me something to shoot for. Just knowing, that I'm gonna be around to se 1985, that I'm gonna succeed in this. That I'm gonna have a chance to travel through time. It's going to be really hard waiting 30 years before I could talk to you about everything that's happened in the past few days. I'm really gonna miss you, Marty."
-    },{
-        name: "Busey Ipsum",
-        description: "Busey ipsum dolor sit amet. These kind of things only happen for the first time once.It's good to yell at people and tell people that you're from Tennesee, so that way you'll be safe.The best way to communicate is compatible. Compatible communication is listening with open ears and an open mind, and not being fearful or judgemental about what you're hearing."
-    },{
-        name: "Samuel L Ipsum",
-        description: "Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing."
-    },{
-        name: "Kitty Ipsum",
-        description: "Chase ball of string eat plants, meow, and throw up because I ate plants going to catch the red dot today going to catch the red dot today. I could pee on this if I had the energy. Chew iPad power cord steal the warm chair right after you get up for purr for no reason leave hair everywhere, decide to want nothing to do with my owner today."
-    }]
+  const [classList, setClassList] = useState(null);
 
-    return ( <div id="home">
+  useEffect(() => {
+    const fakeDelay = setTimeout(() => {
+      Promise.resolve()
+        .then(() => setClassList(dummyClassList))
+        .catch((err) => console.error(err));
+    }, fakeDelayDuration);
 
-        {/* Simple view for MVP */}
+    return () => clearTimeout(fakeDelay);
+  }, []);
 
-            {/* list all classes with edit buttons enabled for instructors */}
+  return (
+    <div id="home">
+      {/* Simple view for MVP */}
+      <h1>Available Classes</h1>
+      {classList ? (
+        classList.map((listItem, index) => (
+          <ClassDetail
+            key={index}
+            name={listItem.name}
+            description={listItem.description}
+          />
+        ))
+      ) : (
+        <div className="loading">
+          <h2>Getting list of classes...</h2>
+        </div>
+      )}
 
-        {/* My Classes view for instructors */}
+      {/* list all classes with edit buttons enabled for instructors */}
 
-            {/* add/edit/remove class functionality - look into pre-built toolbars in react-bootstrap */}
+      {/* My Classes view for instructors */}
 
-            {/* populate with filtered data */}
+      {/* add/edit/remove class functionality - look into pre-built toolbars in react-bootstrap */}
 
-            {/* import ClassDetail component for each class  */}
+      {/* populate with filtered data */}
 
-        {/* Users view */}
+      {/* import ClassDetail component for each class  */}
 
-            {/* list of enrolled classes */}
+      {/* Users view */}
 
-            {/* list of upcoming classes with dropdown view filter */}
+      {/* list of enrolled classes */}
 
-    </div> );
-}
- 
+      {/* list of upcoming classes with dropdown view filter */}
+    </div>
+  );
+};
+
 export default Home;
