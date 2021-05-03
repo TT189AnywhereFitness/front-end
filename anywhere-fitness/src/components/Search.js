b4a674bda4e39ae2a22bb57e351230738dd1f200
Greg Brewton <gbrewton1980@gmail.com>
2021-05-02 19:09:26 -0600

    Displays unfiltered list in Search

--- anywhere-fitness/src/components/Search.js
+++ anywhere-fitness/src/components/Search.js
@@ -1,7 +1,29 @@
 import React from "react";
+import ClassDetail from "./ClassDetail";
+import { FlexRowDiv } from "./styled";
 
 const Search = (props) => {
-  return <div id="search">Search Component</div>;
+  const { classList } = props;
+  return (
+    <FlexRowDiv>
+      {classList.map((listItem, index) => (
+        <ClassDetail
+          key={index}
+          class_id={listItem.class_id}
+          class_name={listItem.class_name}
+          type={listItem.type}
+          start_time={listItem.start_time}
+          duration={listItem.duration}
+          occasion={listItem.occasion}
+          day={listItem.day}
+          intensity={listItem.intensity}
+          location={listItem.location}
+          max_size={listItem.max_size}
+          instructor={listItem.instructor}
+        />
+      ))}
+    </FlexRowDiv>
+  );
 };
 
 export default Search;
